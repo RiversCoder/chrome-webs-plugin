7cb8341f9f0a800c862a86f99fd907c01d00dadf
maozeyu <5400230+maozeyu@user.noreply.gitee.com>
2019-12-26 15:27:39 +0800

    new

--- src/content/index.js
+++ src/content/index.js
@@ -8,21 +8,25 @@ Vue.use(ElementUI);
 const content_script = ` 
     <div id="WebSubscription"></div>
     <style type="text/css">
-        /* @import url("https://cdn.bootcss.com/element-ui/2.12.0/theme-chalk/index.css");*/
+        /* @import url("https://cdn.bootcss.com/element-ui/2.12.0/theme-chalk/index.css"); */
+        .ws-grab-class{
+            border:2px solid red !important;box-sizing: border-box;
+        }
         .el-select-dropdown{
             z-index: 9999999 !important;
-          }
+        }
     </style>
 `;
 
 $(document).ready(()=>{  
     $('body').append(content_script);
     // 等页面加载完成后执行执行
-    setTimeout(() => {
+    const timer = setTimeout(() => {
         new Vue({
             el: '#WebSubscription',
             render: h => h(App)
-        })
+        });
+        // clearTimeout(timer)
     },500);
 });
 
