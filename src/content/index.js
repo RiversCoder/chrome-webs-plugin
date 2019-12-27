073a89289c4d9d4335550a892d7ec1c4912c4dec
maozeyu <5400230+maozeyu@user.noreply.gitee.com>
2019-12-27 21:03:48 +0800

    new

--- src/content/index.js
+++ src/content/index.js
@@ -12,9 +12,13 @@ const content_script = `
     <div id="WebSubscription"></div>
     <style type="text/css">
         /* @import url("https://cdn.bootcss.com/element-ui/2.12.0/theme-chalk/index.css"); */
-        .ws-grab-class{
+        .ws-grab-class, .ws-grab-class-title, .ws-grab-class-author, .ws-grab-class-content, .ws-grab-class-createdAt{
             border:2px solid red !important;box-sizing: border-box;
         }
+        .ws-grab-class-hover{
+            border:2px solid orange !important;box-sizing: border-box;
+        }
+
         .el-select-dropdown{
             z-index: 9999999 !important;
         }
